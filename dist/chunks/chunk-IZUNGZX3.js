24be55d6dda414256eec35f5889082d5aad4eb76
zarif-webflow <zarif.webflow@gmail.com>
2025-11-11 19:01:07 +0600

    add typeform data extractor logic

---
+++ dist/chunks/chunk-IZUNGZX3.js
@@ -0,0 +1,3 @@
+// bin/live-reload.js
+new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
+//# sourceMappingURL=chunk-IZUNGZX3.js.map
--- src/features/report-form.ts
+++ src/features/report-form.ts
@@ -1,14 +1,50 @@
-import "@typeform/embed/build/css/widget.css";
-
 import { getHtmlElement } from "@taj-wf/utils";
-import { createWidget } from "@typeform/embed";
+
+const getFormData = async (responseId: string, apiURL: string) => {
+  try {
+    const response = await fetch(`${apiURL}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ formResponseId: responseId }),
+    });
+    if (!response.ok) {
+      const data = await response.json();
+      console.error("Error while fetching form data for GTM:", data.error);
+      return;
+    }
+    const data = await response.json().then((res) => res.data);
+
+    return data as { name: string; email: string; phoneNumber: string };
+  } catch {
+    throw new Error("Something went wrong while fetching form data for Google Tag Manager!");
+  }
+};
 
 const init = () => {
-  const containerEl = getHtmlElement({ selector: "[data-multistep=container]" });
+  const apiUrl = getHtmlElement({ selector: "[data-api-url]" })?.getAttribute("data-api-url");
+  if (!apiUrl) {
+    console.error("API URL not found in data-api-url attribute");
+    return;
+  }
 
-  if (!containerEl) return;
+  window.addEventListener("message", async (event) => {
+    if (event.origin !== "https://form.typeform.com") return;
 
-  createWidget("jMnpzdy6", { container: containerEl });
+    if (!event.data) {
+      console.error("No data in message event from Typeform");
+      return;
+    }
+
+    if (event.data.type === "form-submit") {
+      const responseId = event.data.responseId;
+
+      const formData = await getFormData(responseId, apiUrl);
+
+      if (!formData) return;
+    }
+  });
 };
 
 init();
